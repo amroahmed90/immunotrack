c96a780f1a78e4bb41f51df0b0f113eef0d1c352
Amro Ahmed <amro.utp.pe@gmail.com>
2021-04-15 02:29:01 +0200

    building the countries search button

--- app.py
+++ app.py
@@ -39,28 +39,31 @@ def index():
         # getting the list of countries in the online database
         url_locations = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/locations.csv"
         # storing the list of countries
-        locations_page = urllib.request.urlopen(
-            url_locations)
-        locations_reader = csv.reader(io.TextIOWrapper(locations_page))
+        locations_page = urllib.request.urlopen(url_locations)
+        locations_page_lines = [l.decode('utf-8')
+                                for l in locations_page.readlines()]
+        locations_reader = csv.reader(locations_page_lines)
         country_list = []
         for location in locations_reader:
-            if location[0] != 'location':
+            if location[0] != 'location':  # to skip the first row (header)
                 country_dict = {}
                 country_dict['country'] = location[0]
                 country_dict['vaccines'] = location[2]
                 country_dict['source_name'] = location[4]
                 country_dict['source_link'] = location[5]
-                # country_list.append(location[0])
+                country_list.append(location[0])
                 # creating a dict with the country info
-                url_country_info = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/" + \
-                    location[0].replace(" ", "%20") + ".csv"
-                country_page = urllib.request.urlopen(url_country_info)
-                country_reader = csv.reader(io.TextIOWrapper(country_page))
-                for data in country_reader:
-                    country_dict['total_vaccinations'] = data[4]
-                    country_dict['people_vaccinated'] = data[5]
-                    country_dict['people_totally_vaccinated'] = data[6]
-                country_list.append(country_dict)
+                # url_country_info = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/" + \
+                #     location[0].replace(" ", "%20") + ".csv"
+                # country_page = urllib.request.urlopen(url_country_info)
+                # country_page_lines = [l.decode('utf-8')
+                #                       for l in country_page.readlines()]
+                # country_reader = csv.reader(country_page_lines)
+                # for data in country_reader:
+                #     country_dict['total_vaccinations'] = data[4]
+                #     country_dict['people_vaccinated'] = data[5]
+                #     country_dict['people_totally_vaccinated'] = data[6]
+                # country_list.append(country_dict)
         return render_template("index.html", country_list=country_list)
     else:
         return render_template("index.html")
